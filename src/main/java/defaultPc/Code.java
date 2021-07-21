1f268ace6f5e73a783c6caa15da41f5834a7b9cb
Azrias <Azrias>
2021-07-21 17:57:25 +0300

    clean up and add TODO

--- src/main/java/defaultPc/Code.java
+++ src/main/java/defaultPc/Code.java
@@ -10,10 +10,10 @@ public final class Code {
     private static final Code CODE = new Code();
     private CommandType type;
 
-    private HashMap<String,String> mapcmd;
-    private HashMap<String,String> mapjump;
-    private HashMap<String,String> mapmem;
-    private String memAloc;
+    private HashMap<String,String> mapCmd;
+    private HashMap<String,String> mapJump;
+    private HashMap<String,String> mapMem;
+    private String memAlloc;
     private String cmd;
     private String jump;
 
@@ -56,9 +56,9 @@ public final class Code {
 
     @NotNull
     private String buildBin() {
-        String binMem = mapmem.get(memAloc);
-        String binCmd = mapcmd.get(cmd);
-        String binJump = mapjump.get(jump);
+        String binMem = mapMem.get(memAlloc);
+        String binCmd = mapCmd.get(cmd);
+        String binJump = mapJump.get(jump);
         return "111" + binCmd + binMem + binJump;
     }
 
@@ -77,70 +77,72 @@ public final class Code {
             currentCommand = currentCommand + ';';
         }
 
+        //TODO refactor to return Array of strings
         String[] split = currentCommand.split("[=;]");
         String[] splitEnsure = Arrays.copyOf(split,3);
 
-        memAloc = splitEnsure[0];
-        if (memAloc.equals("")){
-            memAloc = null;
+        memAlloc = splitEnsure[0];
+        if (memAlloc.equals("")){
+            memAlloc = null;
         }
         cmd = splitEnsure[1];
         jump = splitEnsure[2];
     }
 
+    //TODO make external file
     private void createMapCmd() {
-        mapcmd = new HashMap<>();
-        mapcmd.put("0", "0101010");
-        mapcmd.put("1", "0111111");
-        mapcmd.put("-1", "0111010");
-        mapcmd.put("D", "0001100");
-        mapcmd.put("A", "0110000");
-        mapcmd.put("M", "1110000");
-        mapcmd.put("!D", "0001101");
-        mapcmd.put("!A", "0110001");
-        mapcmd.put("!M", "1110001");
-        mapcmd.put("-D", "0001111");
-        mapcmd.put("-A", "0110011");
-        mapcmd.put("-M", "1110011");
-        mapcmd.put("D+1", "0011111");
-        mapcmd.put("A+1", "0110111");
-        mapcmd.put("M+1", "1110111");
-        mapcmd.put("D-1", "0001110");
-        mapcmd.put("A-1", "0110010");
-        mapcmd.put("M-1", "1110010");
-        mapcmd.put("D+A", "0000010");
-        mapcmd.put("D+M", "1000010");
-        mapcmd.put("D-A", "0010011");
-        mapcmd.put("D-M", "1010011");
-        mapcmd.put("A-D", "0000111");
-        mapcmd.put("M-D", "1000111");
-        mapcmd.put("D&A", "0000000");
-        mapcmd.put("D&M", "1000000");
-        mapcmd.put("D|A", "0010101");
-        mapcmd.put("D|M", "1010101");
+        mapCmd = new HashMap<>();
+        mapCmd.put("0", "0101010");
+        mapCmd.put("1", "0111111");
+        mapCmd.put("-1", "0111010");
+        mapCmd.put("D", "0001100");
+        mapCmd.put("A", "0110000");
+        mapCmd.put("M", "1110000");
+        mapCmd.put("!D", "0001101");
+        mapCmd.put("!A", "0110001");
+        mapCmd.put("!M", "1110001");
+        mapCmd.put("-D", "0001111");
+        mapCmd.put("-A", "0110011");
+        mapCmd.put("-M", "1110011");
+        mapCmd.put("D+1", "0011111");
+        mapCmd.put("A+1", "0110111");
+        mapCmd.put("M+1", "1110111");
+        mapCmd.put("D-1", "0001110");
+        mapCmd.put("A-1", "0110010");
+        mapCmd.put("M-1", "1110010");
+        mapCmd.put("D+A", "0000010");
+        mapCmd.put("D+M", "1000010");
+        mapCmd.put("D-A", "0010011");
+        mapCmd.put("D-M", "1010011");
+        mapCmd.put("A-D", "0000111");
+        mapCmd.put("M-D", "1000111");
+        mapCmd.put("D&A", "0000000");
+        mapCmd.put("D&M", "1000000");
+        mapCmd.put("D|A", "0010101");
+        mapCmd.put("D|M", "1010101");
     }
 
     private void mapPutJump() {
-        mapjump = new HashMap<>();
-        mapjump.put(null, "000");
-        mapjump.put("JGT", "001");
-        mapjump.put("JEQ", "010");
-        mapjump.put("JGE", "011");
-        mapjump.put("JLT", "100");
-        mapjump.put("JNE", "101");
-        mapjump.put("JLE", "110");
-        mapjump.put("JMP", "111");
+        mapJump = new HashMap<>();
+        mapJump.put(null, "000");
+        mapJump.put("JGT", "001");
+        mapJump.put("JEQ", "010");
+        mapJump.put("JGE", "011");
+        mapJump.put("JLT", "100");
+        mapJump.put("JNE", "101");
+        mapJump.put("JLE", "110");
+        mapJump.put("JMP", "111");
     }
 
     private void mapPutDestination() {
-        mapmem = new HashMap<>();
-        mapmem.put(null, "000");
-        mapmem.put("M", "001");
-        mapmem.put("D", "010");
-        mapmem.put("MD", "011");
-        mapmem.put("A", "100");
-        mapmem.put("AM", "101");
-        mapmem.put("AD", "110");
-        mapmem.put("AMD", "111");
+        mapMem = new HashMap<>();
+        mapMem.put(null, "000");
+        mapMem.put("M", "001");
+        mapMem.put("D", "010");
+        mapMem.put("MD", "011");
+        mapMem.put("A", "100");
+        mapMem.put("AM", "101");
+        mapMem.put("AD", "110");
+        mapMem.put("AMD", "111");
     }
 }
